91ea2b8f477a0881a57cd6d3dd5683c11349d8e6
Harish Ved <ved.harish3@gmail.com>
2016-07-22 11:42:19 +0530

    Wait for page load

--- tests/single_test.php
+++ tests/single_test.php
@@ -8,6 +8,10 @@ class SingleTest extends BrowserStackTest {
         $element = self::$driver->findElement(WebDriverBy::name("q"));
         $element->sendKeys("BrowserStack");
         $element->submit();
+        self::$driver->wait(10, 500)->until(function($driver) {
+          $elements = $driver->findElements(WebDriverBy::id("resultStats"));
+          return count($elements) > 0;
+        });
         $this->assertEquals('BrowserStack - Google Search', self::$driver->getTitle());
     }
 
